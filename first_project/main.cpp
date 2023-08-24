88e7c93b91e60f4ba745098007f00ec65c6b2000
Ingemar Ceicer <Ingemar.Ceicer@lerum.se>
2023-08-24 10:50:10 +0200

    First commit

---
+++ first_project/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  // skärm (cout), utmatningsoperator(<<),
+  // text ("Hello Worls"),
+  // utmatningsoperatorn(<<),
+  // radbrytning(endl), slut på sats(;)
+    cout << "Hello world!" << endl;
+  //  hej vid fel i programmet
+  // returneras ett heltal som inte är 0
+  // till operativsystemet
+    return 0; // om allt går bra returneras 0
+}
