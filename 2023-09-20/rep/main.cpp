1937e12657d26dfa06df31c5debffa12a91e76d3
Ingemar Ceicer <Ingemar.Ceicer@lerum.se>
2023-09-20 17:32:21 +0200

    Lektion

---
+++ 2023-09-20/rep/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //cout << "Hello world!" << endl;
+    int heltal;
+    //cin >> heltal;
+    //cout << heltal;
+    // Numeriska (räkne) operatorer
+    // + - * / %
+    // jämförelseoperatorn ==
+    // jämför det som står till höger
+    // med det som står till vänster
+    // 1 == 1   SANT
+    // 1 == 2   FALSKT
+// OBS! Blanda inte ihop
+// = tildelningsoperatorn
+// == jämförelseoperatorn
+
+    if (2==2){
+            cout << "SANT" << endl;
+    }
+
+    return 0;
+}
