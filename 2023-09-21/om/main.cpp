c4b5b6ed643b674275afcbf2d54122ffb721c393
Ingemar Ceicer <Ingemar.Ceicer@lerum.se>
2023-09-21 10:24:43 +0200

    Lektion

---
+++ 2023-09-21/om/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+     system("chcp 1252");
+    // jämförelseoperatorer
+    // Vad är en operator?
+    // Något som har betydelse för
+    // det som ät till höger om
+    // och vänster om operatorn
+    // Utmatningsoperatorn <<
+    // Inmatningsoperatorn >>
+    // tildelningsoperatorn =
+    // jämförelseoperatorer
+    // 5 < 4  "Mindre än"
+    // 5 > 4  "Större än"
+    // 5 == 4 "Lika med"
+    // Om det som står mellan ( och )
+    // är sant utförs det som står mella
+    // { och }
+
+    cout << "Skriv in ett heltal: ";
+    int heltal;
+    cin >> heltal;
+    // inte börja en variabel med en siffra
+    // 1heltal heltal1
+    // my age myAge my_age
+    // undvik åäö
+    if (heltal < 4) {
+       cout << "Är mindre än fyra" << endl;
+    } else {
+                if (heltal < 3) {
+                    cout << "Är mindre än två" << endl;
+                }
+}
+    return 0;
+}
