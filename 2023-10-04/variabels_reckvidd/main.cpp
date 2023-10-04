f637448e81a9dac1bcdf6f156a4b708fd2a1145d
Ingemar Ceicer <Ingemar.Ceicer@lerum.se>
2023-10-04 12:36:20 +0200

    Lektion

---
+++ 2023-10-04/variabels_reckvidd/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   cout << "Mata in ett heltal: " << endl;
+   int tal;
+
+   cin >> tal;
+   if (tal > 5) {
+     int tal2 = 100;
+
+   }
+
+   cout << tal << endl;
+   cout << tal2 << endl;
+
+
+    return 0;
+}
---
+++ 2023-10-04/while/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // 1. Kollar om true
+    // 2. Utför alla satser i while-loopen
+    // 3. Kollar om true
+    // 4. och så vidare
+    int counter=0;
+    while (counter < 10) {
+          cout << "HEJ" << endl;
+          cout << "DU" << endl;
+          // counter är lika med sitt
+          // nuvarande värde plus 1
+          // counter = counter + 1;
+          counter++;
+
+    }
+    return 0;
+}
