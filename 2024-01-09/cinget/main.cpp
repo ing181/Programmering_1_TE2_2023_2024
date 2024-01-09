76ff1d2d61c0bd21800c17e85a936a335b220c01
Ingemar Ceicer <Ingemar.Ceicer@lerum.se>
2024-01-09 12:45:45 +0100

    cin.get()

--- 2024-01-09/cinget/main.cpp
+++ 2024-01-09/cinget/main.cpp
@@ -6,10 +6,10 @@ int main()
     // en array med 5 st. char
     char tecken[5];
     cin.get(tecken, 3);  // Väntar på användarens inmatning
-    // antag att användaren matar in abcdef
+    // antag att användaren matar in abcde
     cout << tecken << endl; // Utskriften blir ab
     // Varför?
-    // sin.get(tecken 3); anger att 3 hamnar i variabeln
+    // sin.get(tecken 3); anger att 3 tecken (char) hamnar i variabeln
     // Vilka tre tecken?
     // ab och '\0'
     // Vad används '\0' till?
@@ -17,6 +17,7 @@ int main()
     // tills den kommer till första '\0' (nolltecken)
     // Man skiljer på
     // C-strängar, igentligen arrayer av char
+	// Kan kallas: null terminerade stängar
     // och std::string
     // vi har inte gått igenom C-strängar.
     // För att föränkla användadet av dessa C-strängar så
